e7ba14300ec2ff78e562454d0e473fa88a39620c
unknown <2120367@talnet.nl>
2019-09-25 11:59:25 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,19 @@
+<?php
+
+$getal1;
+$getal2;
+$resultaat;
+
+echo 'Noem een getal: ';
+
+$getal1 = readline();
+
+echo 'Noem nog een getal: ';
+
+$getal2 = readline();
+
+$resultaat = $getal1 * $getal2;
+
+echo $getal1 . ' x ' . $getal2 . ' = ' . $resultaat;
+
+ ?>
